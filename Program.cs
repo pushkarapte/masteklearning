ebadbc19665a04ebf655b601c0d307608b9d3082
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-11 21:26:32 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+//Namespace declared at the top so that it doesn't coincide with other declarations
+using System;
 
 namespace IntroductiontoCsharpMastekLearning
 {
     class Program
     {
         static void Main1()
-        {
+        {   
+            //Writes output to the console 
             Console.WriteLine("Welcome to C# Mastek Training part 1");
         }
         static void Main()
