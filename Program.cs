92623ce3e011d4a39295cbff18e06cf314ee9541
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-11 20:21:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroductiontoCsharpMastekLearning
+{
+    class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Welcome to C# Mastek Training part 1");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Welcome to C# Mastek Training");
+            Main1();
+        }
+    }
+}
