c720fa2468b96ec3fa33e0e2aaee28e094618431
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-14 19:53:18 +0530

    Add files via upload

--- Program3.cs
+++ Program3.cs
@@ -1,5 +1,5 @@
 ﻿
-//Various operators 
+//Various operators & Nullable types & Null coalescing
 using System;
 
 namespace Operatorsincsharp
@@ -8,6 +8,8 @@ namespace Operatorsincsharp
     {
         static void Main()
         {
+
+
             float num = 20;
             float denom = 40;
             float result = num / denom;
@@ -34,20 +36,52 @@ namespace Operatorsincsharp
                 Console.WriteLine("Match");
             }
 
-            int Num = 44;
+            int Num = 4;
             bool IsNum44 = Num == 54 ? true : false;
 
-           /* if (Num == 44)
+            /* if (Num == 44)
+             {
+                 IsNum44 = true;
+             }
+             else
+             {
+                 IsNum44 = false;
+             }
+             */
+            Console.WriteLine("number == 44 is {0}", IsNum44);
+           
+
+            // Nullable types example
+            
+            bool? AreYouMajor = null;
+
+            if (AreYouMajor == true)
             {
-                IsNum44 = true;
+                Console.WriteLine("He is a major");
+            }
+            else if (AreYouMajor == false)
+            {
+                Console.WriteLine("He is not a major");
             }
             else
             {
-                IsNum44 = false;
+                Console.WriteLine("Did not answer the question");
             }
-            */
-            Console.WriteLine("number == 44 is {0}", IsNum44);  
-        
+
+            int? TOS = null;
+            int AT = TOS ?? 0;
+
+           /* if (TOS == null)
+            {
+                AT = 0;
+            }
+            else
+            {
+                AT = (int)TOS;
+
+            }
+             */
+            Console.WriteLine("AT are {0}", AT);
         }
 
     }
