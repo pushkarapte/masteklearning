72b23dd8029fa610cc2dae8f5a33c65b9e392c71
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-16 22:09:45 +0530

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,44 @@
+﻿
+//Classes in C#
+using System;
+
+class Customer
+{
+    string _firstname;
+    string _lastname;
+
+
+     public Customer()
+         : this("No firstname provided","No lastname provided")
+     {
+     }
+
+    public Customer(string FirstName, string LastName)
+    {
+
+        this._firstname = FirstName;
+        this._lastname = LastName;
+    }
+
+    public void PrintName()
+    {
+        Console.WriteLine("Full Name -> {0}", this._firstname + " " + this._lastname);
+    }
+
+    ~Customer()
+    { 
+        //Clean up code
+    }
+
+}
+
+class Program
+    {
+        static void Main()
+        {
+            //Customer C1 = new Customer();
+            Customer C1 = new Customer("Pushkar","Apte");
+            C1.PrintName();
+        }
+    }
+
