913e9bd1fd60e52b8546d656cfd363da1e23661c
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-16 19:03:15 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,121 @@
+﻿
+// Methods & Namespaces in C sharp
+using System;
+class Program
+{
+    public static void Main()
+    {
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* namespace Methods
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] Numbers = new int[4];
+            Numbers[0] = 1;
+            Numbers[1] = 2;
+            Numbers[2] = 3;
+            Numbers[3] = 4;
+
+            //ParamsMethod();
+            ParamsMethod(Numbers);
+        }
+        public static void ParamsMethod(params int[] Numbers)
+        {
+
+            Console.WriteLine("There are {0} elements ", Numbers.Length);
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+} */
+
+
+
+
+
+
+        //    }    int Total = 0;
+    //        int Product = 0;
+    //        Calculate(45, 50, out Total, out Product);
+    //        Console.WriteLine("Sum = {0} && Product = {1}", Total, Product);
+        
+    //    public static void Calculate(int FN, int SN, out int SUM,out int PROD)
+    //{
+    //    SUM = FN + SN;
+    //    PROD = FN * SN;
+    //}
+//int i = 0;
+//SM(ref i);
+//Console.WriteLine(i);
+
+
+//public static void SM(ref int j)
+//{
+//     j = 101;
+//}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            /* Program.EvenNumbers(); for static method EvenNumbers
+            Program p = new Program();
+            p.EvenNumbers(30);
+           int Sum =  p.Add(10, 20);
+           Console.WriteLine("Sum = {0}", Sum);
+        }
+
+        public int Add(int FN, int SN)
+        {
+            return FN + SN;
+        }
+
+
+
+        public void EvenNumbers(int Target)
+        {
+            int Start = 0;
+            while (Start <= Target)
+            {
+                Console.WriteLine(Start);
+                Start += 2;
+            } */
+
+        
