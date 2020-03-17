a5e411e43bd7b1aa7a680d97b4b5041e198f681a
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-17 18:58:38 +0530

    Add files via upload

--- Program6.cs
+++ Program6.cs
@@ -1,8 +1,124 @@
 ﻿
-//Classes in C#
+//Classes Static Instance Inheritance in C#
 using System;
 
-class Customer
+public class ParentClass
+{
+    public ParentClass()
+    {
+        Console.WriteLine("Parent Constructor");
+    }
+    public ParentClass(string Message)
+    {
+        Console.WriteLine(Message);
+    }
+}
+public class ChildClass : ParentClass 
+{   
+    public ChildClass() : base("Parameterized constructor from base class is called instead of default")
+    {
+        Console.WriteLine("Child Constructor");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        ChildClass CC = new ChildClass();
+        
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ public class Employee
+{
+    public string firstname;
+    public string lastname;
+    public string email;
+                     
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(firstname + " " + lastname);
+        Console.WriteLine(email);
+        
+    }
+}
+
+public class FullTimeEmployee : Employee
+{
+    public float YearlySalary;
+}
+
+public class PartTimeEmployee : Employee
+{
+    public float HourlySalary;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        FullTimeEmployee FTE = new FullTimeEmployee();
+        FTE.firstname = "Pushkar";
+        FTE.lastname = "Apte";
+        FTE.email = "xyz@gmail.com";
+        FTE.YearlySalary = 500000;
+        FTE.PrintFullName();
+       
+    }
+} */
+
+/*class Circle
+{
+    static float _Pi;
+    int _Radius;
+
+    static Circle()
+    {
+        Console.WriteLine("Static field");
+        Circle._Pi = 3.141F;
+    }
+
+public Circle(int Radius)
+{
+    Console.WriteLine("Instance field");
+this._Radius = Radius;
+} 
+public float CalculateArea()
+{
+return _Pi * this._Radius * this._Radius;
+}
+}
+class Program 
+{
+    public static void Main()
+    {
+        Circle c1 = new Circle(10);
+        float Area1 = c1.CalculateArea();
+        Console.WriteLine("Area1 = {0}", Area1);
+
+        Circle c2 = new Circle(15);
+        float Area2 = c2.CalculateArea();
+        Console.WriteLine("Area2 = {0}", Area2);
+    }
+}
+*/
+
+/* class Customer
 {
     string _firstname;
     string _lastname;
@@ -41,4 +157,4 @@ class Program
             C1.PrintName();
         }
     }
-
+*/
