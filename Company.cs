7efc1f0e12b7885631433235e70009be5f7a4992
pushkarapte <43265540+pushkarapte@users.noreply.github.com>
2020-03-21 21:43:50 +0530

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    public class Company
+    {
+        private List<Employee> listEmployees;
+        public Company()
+        {
+            listEmployees = new List<Employee>();
+            listEmployees.Add(new Employee { EmployeeId = 1, Name = "Pushkar", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeId = 2, Name = "Payal", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeId = 3, Name = "Harsh", Gender = "Male" });
+        }
+        
+        public string this[int EmployeeId]
+        {
+            get
+            {
+                return this.listEmployees.FirstOrDefault(emp => emp.EmployeeId == EmployeeId).Name;
+            }
+            set
+            {
+                listEmployees.FirstOrDefault(emp => emp.EmployeeId == EmployeeId).Name = value;
+            }
+        }
+    
+        }
+}
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -10,16 +10,34 @@ namespace WebApplication1
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            Session["Session 1"] = "Session 1 Data";
-            Session["Session 2"] = "Session 2 Data";
-            Response.Write("Retrieve session key using integral indexer");
+        {  
+            Company C = new Company();
+            Response.Write("Name of the employee with id =1" + "  " + C[1]);
             Response.Write("<br/>");
-            Response.Write("Session 1 Data -> " + Session[0].ToString());
+            Response.Write("Name of the employee with id =2" + "  " +C[2]);
             Response.Write("<br/>");
-            Response.Write("Retrieve session key using session indexer");
-            Response.Write("<br/>");
-            Response.Write("Session 2 Data -> " + Session["Session 2"].ToString());
+            Response.Write("Name of the employee with id =3" + "  " +C[3]);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //Session["Session 1"] = "Session 1 Data";
+            //Session["Session 2"] = "Session 2 Data";
+            //Response.Write("Retrieve session key using integral indexer");
+            //Response.Write("<br/>");
+            //Response.Write("Session 1 Data -> " + Session[0].ToString());
+            //Response.Write("<br/>");
+            //Response.Write("Retrieve session key using session indexer");
+            //Response.Write("<br/>");
+            //Response.Write("Session 2 Data -> " + Session["Session 2"].ToString());
         }
 
     }
