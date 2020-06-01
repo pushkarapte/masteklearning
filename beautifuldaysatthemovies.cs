f1cedfc8ea0bed0aaed523fdd26d19be66be4a51
PushkarApte <43265540+pushkarapte@users.noreply.github.com>
2020-06-01 16:48:50 +0530

    Create beautifuldaysatthemovies.cs

---
+++ beautifuldaysatthemovies.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the beautifulDays function below.
+    static int beautifulDays(int i, int j, int k) {
+        var bDays = 0;
+        for(int c = i; c <= j;c++)
+        {
+            var reverseString = new string(c.ToString().Reverse().ToArray());
+            var reverseNumber = int.Parse(reverseString);
+            var d = Math.Abs(c - reverseNumber);
+            var r = d % k;
+            if (r == 0)
+                bDays++;
+        }
+        return bDays;
+
+
+        }
+
+
+    
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] ijk = Console.ReadLine().Split(' ');
+
+        int i = Convert.ToInt32(ijk[0]);
+
+        int j = Convert.ToInt32(ijk[1]);
+
+        int k = Convert.ToInt32(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
