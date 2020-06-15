9fa136ef6513cd927f745645c84b329ef16ab190
PushkarApte <43265540+pushkarapte@users.noreply.github.com>
2020-06-15 12:25:21 +0530

    Create jumpingonthecloud.cs

---
+++ jumpingonthecloud.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+        var emmasPosition =0;
+        var jumpCount = 0;
+        while (true)
+        {
+          if(emmasPosition + 2 <= c.Length -1 && c[emmasPosition + 2] ==0)
+             emmasPosition = emmasPosition +2;
+
+             else
+             emmasPosition++;
+             jumpCount++;
+
+             if(emmasPosition == c.Length -1)
+             break;
+
+        }
+        return jumpCount;
+
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
